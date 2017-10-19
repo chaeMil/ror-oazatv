35864819dac127e2211aa8593274586c0b68fd3e
Michal Mlejnek <michal.mlejnek@juicymo.cz>
2017-10-19 17:23:16 +0200

    Migration

--- app/models/archive_item.rb
+++ app/models/archive_item.rb
@@ -1,2 +1,18 @@
+# == Schema Information
+#
+# Table name: archive_items
+#
+#  id          :integer          not null, primary key
+#  name        :string
+#  description :text
+#  published   :boolean
+#  hash        :string
+#  date        :date
+#  tags        :text
+#  note        :text
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class ArchiveItem < ApplicationRecord
 end
