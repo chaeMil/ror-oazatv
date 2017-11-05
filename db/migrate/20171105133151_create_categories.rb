cd607c25d62c205ab0f5ced15c36c938b5cc81a7
Michal Mlejnek <michal.mlejnek@juicymo.cz>
2017-11-05 14:33:30 +0100

    Add Category model

---
+++ db/migrate/20171105133151_create_categories.rb
@@ -0,0 +1,10 @@
+class CreateCategories < ActiveRecord::Migration[5.1]
+  def change
+    create_table :categories do |t|
+      t.string :title
+      t.string :color
+
+      t.timestamps
+    end
+  end
+end
