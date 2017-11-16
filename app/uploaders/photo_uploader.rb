3a2a239607940d2996bd57192ab6ceda921971ef
Michal Mlejnek <michal.mlejnek@juicymo.cz>
2017-11-16 19:24:53 +0100

    Add small size

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -55,6 +55,10 @@ class PhotoUploader < CarrierWave::Uploader::Base
     process resize_to_fill: [512, 512]
   end
 
+  version :tiny do
+    process resize_to_fit: [256, 256]
+  end
+
   version :small do
     process resize_to_fit: [512, 512]
   end
