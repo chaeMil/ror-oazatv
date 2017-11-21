6dd32bec7afd4fd28a922ea051166eab7c1e1ec0
Michal Mlejnek <michal.mlejnek@juicymo.cz>
2017-11-21 09:57:48 +0100

    Remove nonsense code

--- app/controllers/admin/photos_controller.rb
+++ app/controllers/admin/photos_controller.rb
@@ -1,9 +1,5 @@
 module Admin
   class PhotosController < AdminController
-    if params[:images]
-      params[:images].each do |image|
-        @photo_album.photos.create(image: image)
-      end
-    end
+
   end
 end
