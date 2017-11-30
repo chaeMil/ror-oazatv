99216091799ad41b027391f536964bde5f4e08bd
Michal Mlejnek <michal.mlejnek@juicymo.cz>
2017-11-30 17:34:32 +0100

    Add conditional variants only for images Deleting them not working...

--- app/uploaders/archive_file_uploader.rb
+++ app/uploaders/archive_file_uploader.rb
@@ -1,58 +1,48 @@
 class ArchiveFileUploader < CarrierWave::Uploader::Base
+  include CarrierWave::MiniMagick
 
-  # Include RMagick or MiniMagick support:
-  # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
-
-  # Choose what kind of storage to use for this uploader:
   storage :file
-  # storage :fog
 
-  # Override the directory where uploaded files will be stored.
-  # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
     "uploads/videos/#{model.archive_item_id}/"
   end
-
-  # Provide a default URL as a default if there hasn't been a file uploaded:
-  # def default_url(*args)
-  #   # For Rails 3.1+ asset pipeline compatibility:
-  #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-  #
-  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
-  # end
-
-  # Process files as they are uploaded:
-  # process scale: [200, 300]
-  #
-  # def scale(width, height)
-  #   # do something
-  # end
-
-  # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process resize_to_fit: [50, 50]
-  # end
-
-  # Add a white list of extensions which are allowed to be uploaded.
-  # For images you might use something like this:
-  # def extension_whitelist
-  #   %w(jpg jpeg gif png)
-  # end
-
-  # Override the filename of the uploaded files:
-  # Avoid using model.id or version_name here, see uploader/store.rb for details.
-  # def filename
-  #   "something.jpg" if original_filename
-  # end
 
   def filename
     random_string
   end
 
+  version :thumb, if: :image? do
+    process resize_to_fill: [256, 256]
+  end
+
+  version :thumb_big, if: :image? do
+    process resize_to_fill: [512, 512]
+  end
+
+  version :tiny, if: :image? do
+    process resize_to_fit: [256, 256]
+  end
+
+  version :small, if: :image? do
+    process resize_to_fit: [512, 512]
+  end
+
+  version :medium, if: :image? do
+    process resize_to_fit: [1024, 1024]
+  end
+
+  version :big, if: :image? do
+    process resize_to_fit: [2048, 2048]
+  end
+
   protected
   def random_string
     @string ||= "#{SecureRandom.hex(6)}.#{file.extension}"
+  end
+
+  private
+  def image?(new_file)
+    new_file.content_type.include? 'image'
   end
 
 end
