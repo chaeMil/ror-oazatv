405fbcbe0c2c6f0223d2dee9a732cb08a5850bbb
Michal Mlejnek <michal.mlejnek@juicymo.cz>
2017-12-07 17:46:40 +0100

    Add method for getting video info

--- app/services/archive_file_service.rb
+++ app/services/archive_file_service.rb
@@ -1,20 +1,5 @@
 class ArchiveFileService
-
-  def self.upload_file(archive_item_id, file, filetype)
-    case filetype
-      when ArchiveFile::TYPES[:image]
-        upload_image_file(archive_item_id, file)
-    end
-  end
-
-  def self.generate_file_name(file)
-    "#{SecureRandom.hex(8)}#{File.extname(file.original_filename)}"
-  end
-
-  def self.upload_image_file(archive_item_id, file)
-    #filename = generate_file_name(file)
-    uploader = ArchiveFileImageUploader.new
-    #p filename
-    #p archive_item_id
+  def self.get_video_info(file_path)
+    FFMPEG::Movie.new(file_path)
   end
 end
