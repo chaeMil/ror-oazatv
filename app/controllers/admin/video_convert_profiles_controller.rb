3289e840df4d0214e0608b3fc9fdeeef500c9dc3
Michal Mlejnek <chaemil72@gmail.com>
2017-12-28 18:44:44 +0100

    Add video convert profile layouts with form

--- app/controllers/admin/video_convert_profiles_controller.rb
+++ app/controllers/admin/video_convert_profiles_controller.rb
@@ -1,21 +1,41 @@
 module Admin
   class VideoConvertProfilesController < AdminController
+    before_action :set_profile, only: [:show, :edit, :update, :destroy]
     def index
+      @profiles = VideoConvertProfile.all
     end
 
     def new
+      @profile = VideoConvertProfile.new
     end
 
     def create
-    end
-
-    def show
+      @profile = VideoConvertProfile.new(profile_params)
+      if @profile.save!
+        redirect_to admin_video_convert_profile_path(@profile), notice: 'Video convert profile created successfully.'
+      else
+        render :new
+      end
     end
 
     def update
+      if @profile.update(profile_params)
+        redirect_to admin_video_convert_profile_path(@profile), notice: 'Video convert profile was successfully updated.'
+      else
+        render :edit
+      end
     end
 
     def destroy
+    end
+
+    private
+    def set_profile
+      @profile = VideoConvertProfile.find(params[:id])
+    end
+
+    def profile_params
+      params.require(:video_convert_profile).permit(:title, :video_codec, :video_bitrate, :threads, :resolution, :frame_rate, :custom, :audio_sample_rate, :audio_codec, :audio_channels, :audio_bitrate)
     end
   end
 end
