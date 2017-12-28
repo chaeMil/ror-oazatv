534ab361613c85751f10a2d4c7c2d1c9a4656726
Michal Mlejnek <chaemil72@gmail.com>
2017-12-28 15:29:56 +0100

    Add finished and created at column to queue table

--- app/controllers/admin/video_queue_items_controller.rb
+++ app/controllers/admin/video_queue_items_controller.rb
@@ -10,10 +10,13 @@ module Admin
       workers.each do |process_id, thread_id, job|
         if job['queue'] == queue_name
           arguments = job['payload']['args'][0]['arguments']
+          video_convert_progress = VideoConvertProgress.find(arguments[2])
           running_conversion = {
               archive_file: ArchiveFile.find(arguments[1]),
               archive_item: ArchiveItem.find(arguments[0]),
-              progress: VideoConvertProgress.find(arguments[2])
+              created_at: video_convert_progress[:created_at],
+              finished_at: nil,
+              progress: video_convert_progress
           }
           @running_jobs << running_conversion
         end
@@ -26,6 +29,8 @@ module Admin
         queued_job = {
             archive_file: ArchiveFile.find(arguments[1]),
             archive_item: ArchiveItem.find(arguments[0]),
+            created_at: job.created_at,
+            finished_at: nil,
             progress: VideoConvertProgress.find(arguments[2])
         }
         @queued_jobs << queued_job
@@ -38,6 +43,8 @@ module Admin
         finished_job = {
             archive_file: archive_file,
             archive_item: archive_file.archive_item,
+            created_at: job.created_at,
+            finished_at: job.finished_at,
             progress: VideoConvertProgress.status.find_value(:done).value
         }
         @finished_jobs << finished_job
