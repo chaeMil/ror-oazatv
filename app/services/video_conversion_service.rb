5fa4c331176bb3f772b60eb9bfcda6aace00828f
Michal Mlejnek <chaemil72@gmail.com>
2018-01-18 15:50:03 +0100

    Refactor

--- app/services/video_conversion_service.rb
+++ app/services/video_conversion_service.rb
@@ -17,31 +17,45 @@ class VideoConversionService
 
         video_convert_progress.update(archive_file_id: new_archive_file.id, started_at: DateTime.now)
 
-        options = create_transcode_options(convert_profile)
-
-        begin
-          video.transcode("#{File.dirname(archive_file.file.path)}/#{new_video_filename}", options) do |progress|
-            video_convert_progress.update(progress: progress,
-                                          status: VideoConvertProgress.status.find_value(:processing).value)
-            if progress == 1.0
-              video_convert_progress.update(finished_at: DateTime.now,
-                                            status: VideoConvertProgress.status.find_value(:done).value)
-            end
-            p progress
-          end
-        rescue StandardError => error
-          p '=======================error================'
-          p error
-          p '=================error======================='
-          video_convert_progress.update(error: error,
-                                        status: VideoConvertProgress.status.find_value(:error).value)
-        end
+        transcode(archive_file, new_video_filename, video, convert_profile, video_convert_progress)
       else
         throw 'File is not a video! Cannot convert'
       end
     end
 
     private
+    def transcode(archive_file, new_video_filename, video, convert_profile, video_convert_progress)
+      options = create_transcode_options(convert_profile)
+      begin
+        video.transcode("#{File.dirname(archive_file.file.path)}/#{new_video_filename}", options) do |progress|
+          on_transcode_progress(progress, video_convert_progress)
+          if progress == 1.0
+            on_transcode_finish(video_convert_progress)
+          end
+        end
+      rescue StandardError => error
+        on_transcode_error(error, video_convert_progress)
+      end
+    end
+
+    def on_transcode_progress(progress, video_convert_progress)
+      video_convert_progress.update(progress: progress,
+                                    status: VideoConvertProgress.status.find_value(:processing).value)
+    end
+
+    def on_transcode_finish(video_convert_progress)
+      video_convert_progress.update(finished_at: DateTime.now,
+                                    status: VideoConvertProgress.status.find_value(:done).value)
+    end
+
+    def on_transcode_error(error, video_convert_progress)
+      p '=================error======================='
+      p error
+      p '=================error======================='
+      video_convert_progress.update(error: error,
+                                    status: VideoConvertProgress.status.find_value(:error).value)
+    end
+
     def create_new_archive_file(archive_file, archive_item)
       new_archive_file = ArchiveFile.new(archive_item: archive_item,
                                          language_id: archive_file['language_id'],
