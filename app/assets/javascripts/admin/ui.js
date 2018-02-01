a814cc07c84f44ea9afb7bdb5f2c41b65442c17d
Michal Mlejnek <chaemil72@gmail.com>
2018-02-01 17:40:05 +0100

    Add custom toolbar

--- app/assets/javascripts/admin/ui.js
+++ app/assets/javascripts/admin/ui.js
@@ -1,12 +1,20 @@
-$(function () {
+$(document).on('turbolinks:load', function(e) {
     $('[data-toggle="popover"]').popover({
         container: 'body'
     });
 
     $('[data-provider="summernote"]').each(function() {
         $(this).summernote({
-            height: 300
+            height: 300,
+            toolbar: [
+                ["main", ["undo", "redo"]],
+                ["style", ["bold", "italic", "underline", "clear"]],
+                ["font", ["strikethrough", "superscript", "subscript"]],
+                ["para", ["ul", "ol", "paragraph"]],
+                ["insert", ["link"]],
+                ["misc", ["fullscreen"]],
+                ["help"]
+            ]
         });
     });
-
 });
