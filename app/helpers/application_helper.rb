1a0da2c6cf5c52dec1d7240f81ffa9d0ed35ffc9
Michal Mlejnek <michal.mlejnek@juicymo.cz>
2018-02-15 12:01:16 +0100

    Add nav_link helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@ module ApplicationHelper
   def format_video_duration(input)
     Time.at(input).utc.strftime("%H:%M:%S")
   end
+
+  def nav_link(link_text, link_path, li_class, a_class)
+    class_name = current_page?(link_path) ? 'active' : ''
+
+    content_tag(:li, :class => "#{li_class} #{class_name}") do
+      link_to link_text, link_path, class: a_class
+    end
+  end
 end
