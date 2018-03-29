bcfaa48990b9b5b8617d73a64475e0ccb5a9acef
Michal Mlejnek <chaemil72@gmail.com>
2018-03-29 12:33:28 +0200

    Add js routes

---
+++ app/assets/javascripts/js-routes-rails.js
@@ -0,0 +1,51 @@
+var JsRoutesRails = (function() {
+  var routes = {};
+  
+  routes['convert_admin_archive_item_archive_file_path'] = function(options) {
+    return format('/admin/archive_items/:archive_item_id/archive_files/:id/convert', options);
+  };
+  
+  routes['admin_archive_item_archive_files_path'] = function(options) {
+    return format('/admin/archive_items/:archive_item_id/archive_files', options);
+  };
+  
+  routes['new_admin_archive_item_archive_file_path'] = function(options) {
+    return format('/admin/archive_items/:archive_item_id/archive_files/new', options);
+  };
+  
+  routes['edit_admin_archive_item_archive_file_path'] = function(options) {
+    return format('/admin/archive_items/:archive_item_id/archive_files/:id/edit', options);
+  };
+  
+  routes['admin_archive_item_archive_file_path'] = function(options) {
+    return format('/admin/archive_items/:archive_item_id/archive_files/:id', options);
+  };
+  
+  routes['admin_archive_items_path'] = function(options) {
+    return format('/admin/archive_items', options);
+  };
+  
+  routes['new_admin_archive_item_path'] = function(options) {
+    return format('/admin/archive_items/new', options);
+  };
+  
+  routes['edit_admin_archive_item_path'] = function(options) {
+    return format('/admin/archive_items/:id/edit', options);
+  };
+  
+  routes['admin_archive_item_path'] = function(options) {
+    return format('/admin/archive_items/:id', options);
+  };
+  
+
+  function format(string, options) {
+    var str = string.toString();
+    for (var option in options) {
+      str = str.replace(RegExp("\\:" + option, "gi"), options[option]);
+    }
+
+    return str;
+  };
+
+  return routes;
+})();
