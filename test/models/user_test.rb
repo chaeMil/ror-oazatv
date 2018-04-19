20604562b539966bcd82d6636e9c942db65e2e0d
Michal Mlejnek <michal.mlejnek@juicymo.cz>
2018-04-19 13:36:11 +0200

    Add favicon

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -15,6 +15,7 @@
 #  last_sign_in_ip        :string
 #  created_at             :datetime         not null
 #  updated_at             :datetime         not null
+#  admin                  :boolean          default(FALSE)
 #
 # Indexes
 #
