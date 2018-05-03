da94088b11465c9760ce8f56e43e2b2a14cd6969
Michal Mlejnek <michal.mlejnek@juicymo.cz>
2018-05-03 17:35:39 +0200

    clean up code

--- app/assets/javascripts/application/video-player.js
+++ app/assets/javascripts/application/video-player.js
@@ -211,27 +211,4 @@ $(document).on('turbolinks:load', function () {
             }, 500);
         });
     }
-
-    //just a test
-    /*setTimeout(function () {
-        currentTime = player.currentTime;
-        canSeek = false;
-        player.source = {
-            type: 'video',
-            title: 'Test',
-            sources: [
-                {
-                    src: '/uploads/videos/100/04896809c412.mp4',
-                    type: 'video/mp4'
-                }
-            ]
-        };
-        player.play();
-        player.on('playing', function (e) {
-            if (canSeek) {
-                player.currentTime = currentTime;
-                canSeek = false;
-            }
-        });
-    }, 10 * 1000);*/
 });
