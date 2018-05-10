ea4f364368133d788a0d648ef2b312e24085d210
Michal Mlejnek <michal.mlejnek@juicymo.cz>
2018-05-10 13:56:59 +0200

    Refactor

--- app/assets/javascripts/application/video-player.js
+++ app/assets/javascripts/application/video-player.js
@@ -130,12 +130,13 @@ $(document).on('turbolinks:load', function () {
             default:
                 syncFix = 0.25;
         }
+        syncFix += doubleToFloat(audioSourceFileSyncFix);
         var difference = Math.abs(player.currentTime - audio.currentTime + syncFix);
         console.log('difference', difference);
         audio.muted = false;
         if (difference > syncFix) {
             console.warn('syncAudioWithVideo', 'difference bigger than 0.1 (' + difference + '), syncing');
-            audio.currentTime = player.currentTime + syncFix + doubleToFloat(audioSourceFileSyncFix);
+            audio.currentTime = player.currentTime + syncFix;
         }
     }
 
