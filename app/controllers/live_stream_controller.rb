d1e7473a5f040c9dea7f7af6b74897a89506b53e
Michal Mlejnek <michal.mlejnek@juicymo.cz>
2018-05-10 14:29:23 +0200

    Add youtube embedded video

--- app/controllers/live_stream_controller.rb
+++ app/controllers/live_stream_controller.rb
@@ -1,5 +1,14 @@
 class LiveStreamController < ApplicationController
+  require 'moneta'
+  before_action :setup_moneta
+
   def view
 
+  end
+
+  private
+
+  def setup_moneta
+    @store = Moneta.new(:File, dir: 'local_config/livestream')
   end
 end
