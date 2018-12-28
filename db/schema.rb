a3ed2ca2ee913ebd4f941c6fb135d4b05d553f89
Michal Mlejnek <michal.mlejnek@juicymo.cz>
2018-12-28 11:31:03 +0100

    Add Admin model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180510143500) do
+ActiveRecord::Schema.define(version: 20181228101452) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -18,11 +18,6 @@ ActiveRecord::Schema.define(version: 20180510143500) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer "sign_in_count", default: 0, null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string "current_sign_in_ip"
-    t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
